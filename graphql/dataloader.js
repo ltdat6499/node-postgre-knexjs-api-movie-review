d85a9a962a4df11467f97ff9ab118b0ef320061c
Ly Thanh Dat <dat6499proo@gmail.com>
2020-11-03 14:37:10 +0700

    export

--- graphql/dataloader.js
+++ graphql/dataloader.js
@@ -13,13 +13,7 @@ const getLoaders = (table) => ({
   loader: new DataLoader(async (list) => {
     return await list.map(async (ids) => {
       const res = await db(table).whereRaw(`id = ANY(?)`, [ids]);
-      // let result = [];
-      // for (let i = 0; i < ids.length; i++) {
-      //   result[i] = res.find((x) => x.id === ids[i]);
-      // }
-      let result = [];
-      result = mapIds(ids, res);
-      return result;
+      return mapIds(ids, res);
     });
   }),
 });
