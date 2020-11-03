2fd5e4ee6957f0106d1b1efefe24f5470e2ffa64
Ly Thanh Dat <dat6499proo@gmail.com>
2020-11-03 11:34:30 +0700

    oke

--- graphql/dataloader.js
+++ graphql/dataloader.js
@@ -4,7 +4,12 @@ const db = require("../configs/database-connect");
 const getLoaders = (table) => ({
   loader: new DataLoader(async (list) => {
     return await list.map(async (ids) => {
-      return await db(table).whereRaw(`id = ANY(?)`, [ids]);
+      const res = await db(table).whereRaw(`id = ANY(?)`, [ids]);
+      let result = [];
+      for (let i = 0; i < ids.length; i++) {
+        result[i] = res.find((x) => x.id === ids[i]);
+      }
+      return result;
     });
   }),
 });
