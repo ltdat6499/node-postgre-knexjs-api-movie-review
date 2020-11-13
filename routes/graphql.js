8356daa51b38f4dcc5aa8e029a72b52b6a7742b5
Ly Thanh Dat <dat6499proo@gmail.com>
2020-11-13 15:36:44 +0700

    first

--- routes/graphql.js
+++ routes/graphql.js
@@ -3,18 +3,16 @@ const Router = require("koa-router");
 const executableSchema = require("../graphql/relay");
 const router = new Router({ prefix: "/graphql" });
 
-
-
 router.all(
-  "/",
-  koaGraphQL(async (ctx) => {
-    return {
-      schema: executableSchema,
-      graphiql: true,
-      context: {},
-      debug: true,
-    };
-  })
+     "/",
+     koaGraphQL(async (ctx) => {
+          return {
+               schema: executableSchema,
+               graphiql: true,
+               context: {},
+               debug: true,
+          };
+     })
 );
 
 module.exports = router;
